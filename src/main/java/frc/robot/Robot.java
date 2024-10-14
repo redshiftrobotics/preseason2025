fd0bc37604adf458bc2bb29c5e9613b602ed7e69
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2024-10-14 01:49:24 -0700

    add brownout setting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.littletonrobotics.junction.LogFileUtil;
@@ -66,6 +67,9 @@ public class Robot extends LoggedRobot {
 
     // Start AdvantageKit logger
     Logger.start();
+
+    // Set the voltage the roboRIO will brownout and disable all outputs. (Only works on roboRIO 2s)
+    RobotController.setBrownoutVoltage(6.0);
 
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our autonomous chooser on the dashboard.
