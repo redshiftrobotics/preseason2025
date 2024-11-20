6fb0f6a73ddedcfe7be4483e12ca6f1e9e3bc4a5
Aceius E. <aceiuselms@seattleacademy.org>
2024-11-19 16:59:31 -0800

    Mostly done hardware io layer

--- src/main/java/frc/robot/subsystems/gateway/GatewayIOHardware.java
+++ src/main/java/frc/robot/subsystems/gateway/GatewayIOHardware.java
@@ -1,5 +1,31 @@
 package frc.robot.subsystems.gateway;
 
+import frc.robot.hardwareWrappers.RelaySolenoid;
+import frc.robot.hardwareWrappers.Transducer;
+
 public class GatewayIOHardware implements GatewayIO {
-  //
+  private GatewayIOInput input;
+  private RelaySolenoid solenoid;
+  private Transducer transducer;
+
+  public GatewayIOHardware(RelaySolenoid solenoid) {
+    this.solenoid = solenoid;
+  }
+
+  @Override
+  public void updateInputs(GatewayIOInput inputs) {
+    input.psi = (float) transducer.getTankPSI();
+  }
+
+  @Override
+  public void beginFilling() {
+    input.filling = true;
+    solenoid.open();
+  }
+
+  @Override
+  public void stopFilling() {
+    input.filling = false;
+    solenoid.close();
+  }
 }
